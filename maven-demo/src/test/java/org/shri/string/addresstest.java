dd14a60f1b3cc1d6d196bee147309c4eff800e11
Shrishti34 <saxena.shrishti03@gmail.com>
2021-05-14 18:32:38 +0530

    new

---
+++ maven-demo/src/test/java/org/shri/string/addresstest.java
@@ -0,0 +1,14 @@
+package org.shri.string;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class addresstest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ maven-demo/src/test/java/question/questiontest.java
@@ -0,0 +1,14 @@
+package question;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class questiontest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
